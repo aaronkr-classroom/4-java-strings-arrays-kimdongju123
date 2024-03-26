61215b9407757366c74ffa0693f51a96a89cba4a
kimdongju123 <08555@naver.com>
2024-03-26 15:57:37 +0900

    finish

---
+++ src/Ex501.java
@@ -0,0 +1,13 @@
+
+public class Ex501 {
+//My Method
+	public static void hello(String name = "Aaron"); {
+		System.out.println("Hello,"+name);
+		
+		
+		public static void main(String[] args) {
+			//TOD0 Auto-generated method stub
+			hello("Aaron");
+		}
+	
+	}
---
+++ src/ex502.java
@@ -0,0 +1,73 @@
+
+public class ex502 {
+public static double add(double a, double b) {return a+ b;}
+public static double sub(double a, double b) {return a - b;}
+	public static double mult(double a, double b) {return a * b;}
+		public static double div(double a, double b) {return a / b;}
+public static double max(double a, double b) {
+	if (a >=b) return a;
+	else return b;
+}
+public static double min(double a, double b) {
+	if (a <=b) return a;
+	else return b;
+
+
+	}
+
+public static void main(String[] args) {
+	//TOD0 Auto-generated method stub
+	int a= 15;
+	int b= 7;
+	factorial(a);
+	factorial(b);
+	
+	System.out.println("Calculator program:");
+	System.out.println("Add:" + a +"+"+b + "="+ add(a,b));
+	System.out.println("Add:"+"a"+"+"+b + "="+ sub(a,b));
+	System.out.println("Add:"+"a"+"+"+b + "="+ mult(a,b));
+	System.out.println("Add:"+"a"+"+"+b + "="+ div(a,b));
+	
+	System.out.println("max:("+"a +","+ b + "):"+ max(a,b));
+	System.out.println("max:("+"a +","+ b + "):"+ min(a,b));"
+		
+		
+		//factorial
+		public static void factorial(int a) {
+			int result = a;
+			for (int i =1;<= a; i++) {
+				result *+i;
+			}
+	System.out.println("factorial of" + a + "!" + result);
+	//circle
+	publc static void circ(int a) {
+		System.out.println("Pizza size:"+ a +"inches");
+		+ 2 * a * 3.1415792 //Math.PI
+		);
+		
+		publc static void area(int a) {
+			System.out.println(
+				"Pizza size:"+ a +"inches:
+			);	
+		
+	}
+	}
+	public static void main(String[] args) {
+		//TOD0 Auto-generated method stub
+		int a= 15;
+		int b= 7;
+		/Factorial(a);
+		/Factorial(b;)
+		
+		
+	//Circle (pizza) 계산
+		circ(b);
+		area(b);
+	
+		circ(12); //int 메개변수  
+		circ(1.24567); //double 메개변수
+        float c = 12.345f;
+        circ(c);
+	
+	}
+	
